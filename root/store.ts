47732583a27993f0a06b380199143f40c76b60f5
Marvin Luchs <marvin.luchs@holisticon.de>
2019-05-07 21:34:24 +0200

    reset repository

--- root/store.ts
+++
@@ -1,6 +0,0 @@
-import { createOrGetStore } from '@hypercontract/hypercontract-shared';
-import { vocabulary } from './vocabulary';
-
-export function getVocabularyStore() {
-    return createOrGetStore('vocabulary', vocabulary);
-}
